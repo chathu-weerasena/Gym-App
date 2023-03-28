1dd749908cd4c7fbdf4a52eb52cd0c157c3bb631
chathu-weerasena <chathuweerasena@gmail.com>
2023-03-28 22:57:36 +0200

    Navbar with types

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/scenes/navbar";
+import { useState } from "react";
 
 import viteLogo from "/vite.svg";
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState("home");
   return (
     <div className="app bg-gray-20 ">
-      <Navbar />
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </div>
   );
 }
