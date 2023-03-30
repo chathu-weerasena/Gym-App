5f37a2cae5c5ac9d5b6a4862d4f700698bba7ee7
chathu-weerasena <chathuweerasena@gmail.com>
2023-03-30 13:54:38 +0200

    Completed content in Benefits page

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -1,11 +1,33 @@
+import Benefit from "@/shared/Benefit";
 import Heading from "@/shared/Heading";
-import { SelectedPage } from "@/shared/types";
+import { BenefitType, SelectedPage } from "@/shared/types";
 import {
   HomeModernIcon,
   UserGroupIcon,
   AcademicCapIcon,
 } from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
+
+const benefits: Array<BenefitType> = [
+  {
+    icon: <HomeModernIcon className="h-6 w-6" />,
+    title: "State of the Art Facilites",
+    description:
+      "Neque adipiscing amet amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et.",
+  },
+  {
+    icon: <UserGroupIcon className="h-6 w-6" />,
+    title: "100's of Diverse Classes",
+    description:
+      "Eu ipsum id egestas risus tempus enim semper felis quis. Nec consectetur ac venenatis facilisi est. Eget ac turpis id.",
+  },
+  {
+    icon: <AcademicCapIcon className="h-6 w-6" />,
+    title: "Expert and Pro Trainers",
+    description:
+      "Fusce vestibulum aliquam ut cras. Nisl lectus egestas sapien nisl. Lacus at mi sit pellentesque. Congue parturient.",
+  },
+];
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -27,7 +49,17 @@ const Benefits = ({ setSelectedPage }: Props) => {
           </p>
         </div>
         {/* BENEFITS */}
-        <div className="mt-5 items-center justify-between gap-8 md:flex"></div>
+        <div className="mt-5 items-center justify-between gap-8 md:flex">
+          {benefits.map((benefit: BenefitType) => (
+            <Benefit
+              key={benefit.title}
+              icon={benefit.icon}
+              title={benefit.title}
+              description={benefit.description}
+              setSelectedPage={setSelectedPage}
+            />
+          ))}
+        </div>
       </motion.div>
     </section>
   );
