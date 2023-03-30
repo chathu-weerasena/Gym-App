cc7f2b5175815a832aaee704f95f3cb02d112ce6
chathu-weerasena <chathuweerasena@gmail.com>
2023-03-30 13:25:02 +0200

    Initiating Benefits page

---
+++ src/scenes/benefits/index.tsx
@@ -0,0 +1,36 @@
+import Heading from "@/shared/Heading";
+import { SelectedPage } from "@/shared/types";
+import {
+  HomeModernIcon,
+  UserGroupIcon,
+  AcademicCapIcon,
+} from "@heroicons/react/24/solid";
+import { motion } from "framer-motion";
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const Benefits = ({ setSelectedPage }: Props) => {
+  return (
+    <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+      >
+        {/* HEADER */}
+        <div className="md:my-5 md:w-3/5">
+          <Heading>MORE THAN JUST A GYM.</Heading>
+          <p className="my-5 text-sm">
+            We provide world class fitness equipment, trainers and classes to
+            get you to your ultimate fitness goals with ease. We provide true
+            care into each and every member.
+          </p>
+        </div>
+        {/* BENEFITS */}
+        <div className="mt-5 items-center justify-between gap-8 md:flex"></div>
+      </motion.div>
+    </section>
+  );
+};
+
+export default Benefits;
