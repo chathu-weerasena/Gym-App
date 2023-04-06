0ca8d8949570506e56a16d9a67b58bb0444e258f
chathu-weerasena <chathuweerasena@gmail.com>
2023-04-06 19:47:02 +0200

    Initial content of Contact Us page

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -1,12 +1,42 @@
 import { SelectedPage } from "@/shared/types";
-import React from "react";
+import { useForm } from "react-hook-form";
+import { motion } from "framer-motion";
+import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
+import Heading from "@/shared/Heading";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
 
 const ContactUs = ({ setSelectedPage }: Props) => {
-  return <div>ContactUs</div>;
+  return (
+    <section id="contactus" className="mx-auto w-5/6 pt-24">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
+      >
+        {/* HEADER */}
+        <motion.div
+          className="md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        ></motion.div>
+        <Heading>
+          <span className="text-primary-500">JOIN NOW</span> TO GET IN SHAPE
+        </Heading>
+        <p className="my-5">
+          Congue adipiscing risus commodo placerat. Tellus et in feugiat nisl
+          sapien vel rhoncus. Placerat at in enim pellentesque. Nulla adipiscing
+          leo egestas nisi elit risus sit. Nunc cursus sagittis.{" "}
+        </p>
+      </motion.div>
+    </section>
+  );
 };
 
 export default ContactUs;
