a8df0d9d42309e6f22142237edb79fe63e2bcba5
chathu-weerasena <chathuweerasena@gmail.com>
2023-04-07 21:56:01 +0200

    Initiating Footer content

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -3,7 +3,11 @@ import Logo from "@/assests/Logo.png";
 type Props = {};
 
 const Footer = (props: Props) => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-primary-100 py-16">
+      <div className="justify-content mx-auto w-5/6 gap-16 md:flex"></div>
+    </footer>
+  );
 };
 
 export default Footer;
